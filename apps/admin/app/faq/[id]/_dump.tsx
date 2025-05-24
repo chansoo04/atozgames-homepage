dc8e6317445fd3840434e76084468a203127c6f6
chansoo04 <56948241+chansoo04@users.noreply.github.com>
2025-05-24 16:24:17 +0900

    [fix] 자주 묻는질문 정상화

--- apps/admin/app/faq/[id]/_dump.tsx
+++ apps/admin/app/faq/[id]/_dump.tsx
@@ -23,57 +23,50 @@ export default async function Page({ params }: { params: { id: number } }) {
   const faq: any = await getPageData(id);
 
   return (
-    <main className="relative w-full">
+    <main className="w-full desktop:flex desktop:min-h-screen desktop:w-full desktop:flex-col desktop:bg-[url(/bg_desktop.png)] desktop:bg-cover desktop:bg-center desktop:bg-no-repeat">
+      <TopBar />
+
       {/* 모바일(<640)에서만 보임 */}
       <section
-        className="flex min-h-[90vh] flex-col items-center pb-10 pt-[92px] desktop:hidden"
+        className="tablet:hidden mt-[60px] flex min-h-[85vh] flex-col items-center px-5 pb-10 pt-8 desktop:hidden"
         style={{
           background: "linear-gradient(0deg, #F5F5F8 0%, #DCEFFF 100%)",
         }}
       >
-        <TopBar />
         <h1 className="text-lg font-bold leading-relaxed text-gray-700">자주 묻는 질문</h1>
-
-        <div className="px-5 ">
-          <div className="mt-5 w-full rounded-[20px] bg-gray-100 px-5 pb-[30px] pt-5">
-            <div className="py-[15px]">
-              <div
-                className={`inline-block rounded-full px-2.5 text-sm font-semibold ${faq.subcategory === "공지" ? "bg-serve text-gray-100" : "bg-primary text-gray-700"}`}
-              >
-                {faq.subcategory}
-              </div>
-              <div className="mt-2.5 whitespace-pre-line text-base font-medium leading-relaxed text-gray-700">
-                {faq.title}
-              </div>
-              <div className="mt-[15px] flex items-center gap-x-1">
-                <Image src="/clock.png" alt="작성일" width={14} height={14} />
-                <div className="text-sm font-normal text-gray-500">
-                  {changeDate(faq.created_at)}
-                </div>
-              </div>
+        <div className="mt-5 w-full rounded-[20px] bg-gray-100 px-5 pb-[30px] pt-5">
+          <div className="py-[15px]">
+            <div
+              className={`inline-block rounded-full px-2.5 text-sm font-semibold ${faq.subcategory === "공지" ? "bg-serve text-gray-100" : "bg-primary text-gray-700"}`}
+            >
+              {faq.subcategory}
             </div>
-            <div className="whitespace-pre-line py-5 text-sm font-normal leading-tight text-gray-700">
-              {faq.content}
+            <div className="mt-2.5 whitespace-pre-line text-base font-medium leading-relaxed text-gray-700">
+              {faq.title}
             </div>
+            <div className="mt-[15px] flex items-center gap-x-1">
+              <Image src="/clock.png" alt="작성일" width={14} height={14} />
+              <div className="text-sm font-normal text-gray-500">{changeDate(faq.created_at)}</div>
+            </div>
+          </div>
+          <div className="whitespace-pre-line py-5 text-sm font-normal leading-tight text-gray-700">
+            {faq.content}
           </div>
         </div>
       </section>
 
       {/* 데스크탑(≥1024)에서만 보임 */}
-      <section className="relative hidden desktop:block desktop:aspect-[1920/2086] desktop:w-full desktop:bg-[url('/bg_desktop.png')] desktop:bg-[length:100%_auto] desktop:bg-top desktop:bg-repeat-y">
-        <TopBar />
-
-        <div className="desktop:flex desktop:flex-col desktop:items-center desktop:pt-60">
+      <section className="hidden desktop:mt-60 desktop:block desktop:min-h-screen desktop:flex-1">
+        <div className="desktop:mx-auto desktop:flex desktop:w-full desktop:max-w-[1200px] desktop:flex-col">
           <h1 className="desktop:text-center desktop:text-5xl desktop:font-bold desktop:text-white">
             자주 묻는 질문
           </h1>
-
-          <div className="w-[1200px] desktop:mt-20 desktop:flex desktop:flex-col desktop:rounded-[25px] desktop:bg-[#16172D]/70 desktop:px-[50px] desktop:py-5">
+          <div className="desktop:mt-20 desktop:flex desktop:flex-col desktop:rounded-[25px] desktop:bg-[#16172D]/70 desktop:px-[50px] desktop:py-5">
             <div className="flex justify-between pt-10">
               <div
                 className={`flex h-[30px] w-[64px] items-center justify-center rounded-full text-lg font-medium ${faq.category === "공지" ? "bg-serve text-white" : "bg-primary text-gray-700"}`}
               >
-                {faq.subcategory}
+                {faq.category}
               </div>
               <div className="flex items-center gap-x-1">
                 <Image src="/clock.png" alt="작성일" width={20} height={20} />
--- apps/admin/app/faq/_dump.tsx
+++ apps/admin/app/faq/_dump.tsx
@@ -15,150 +15,143 @@ export default function Tabs({ faqs }: { faqs: any }) {
   const [active, setActive] = useState<(typeof tabs)[number]>("게임문의");
 
   return (
-    <main className="relative w-full">
+    <main className="w-full desktop:flex desktop:min-h-screen desktop:w-full desktop:flex-col desktop:bg-[url(/bg_desktop.png)] desktop:bg-cover desktop:bg-center desktop:bg-no-repeat">
+      <TopBar />
+
       {/* 모바일(<640)에서만 보임 */}
       <section
-        className="flex min-h-[90vh] flex-col items-center pb-10 pt-[92px] desktop:hidden"
+        className="mt-[60px] flex min-h-[85vh] flex-col items-center px-5 pb-10 pt-8 desktop:hidden"
         style={{
           background: "linear-gradient(0deg, #F5F5F8 0%, #DCEFFF 100%)",
         }}
       >
-        <TopBar />
         <h1 className="text-lg font-bold leading-relaxed text-gray-700">자주 묻는 질문</h1>
 
-        <div className="px-5 ">
-          <div className="mt-5 flex w-full flex-col rounded-[20px] bg-gray-100 px-5 pb-5 pt-2.5">
-            <nav className="grid grid-cols-3 gap-x-[5px]">
-              {tabs.map((tab) => (
-                <div key={tab} className="relative">
-                  <button
+        <div className="mt-5 flex w-full flex-col rounded-[20px] bg-gray-100 px-5 pb-5 pt-2.5">
+          <nav className="grid grid-cols-3 gap-x-[5px]">
+            {tabs.map((tab) => (
+              <div key={tab} className="relative">
+                <button
+                  onClick={() => setActive(tab)}
+                  className={`w-full py-3 text-center ${active === tab ? "text-base font-semibold text-primary" : "text-base font-normal text-gray-400"}`}
+                >
+                  {tab}
+                </button>
+                {active === tab ? (
+                  <svg
                     onClick={() => setActive(tab)}
-                    className={`w-full py-3 text-center ${active === tab ? "text-base font-semibold text-primary" : "text-base font-normal text-gray-400"}`}
+                    className="absolute bottom-0 left-1/2 -translate-x-1/2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="90"
+                    height="50"
+                    viewBox="0 0 90 50"
+                    fill="none"
                   >
-                    {tab}
-                  </button>
-                  {active === tab ? (
-                    <svg
-                      onClick={() => setActive(tab)}
-                      className="absolute bottom-0 left-1/2 -translate-x-1/2"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="90"
-                      height="50"
-                      viewBox="0 0 90 50"
-                      fill="none"
-                    >
-                      <g opacity="0.21">
-                        <mask
-                          id="mask0_99_2124"
-                          style={{ maskType: "alpha" }}
-                          maskUnits="userSpaceOnUse"
-                          x="0"
-                          y="0"
-                          width="90"
-                          height="50"
-                        >
-                          <rect width="90" height="50" fill="#D9D9D9" />
-                        </mask>
-                        <g mask="url(#mask0_99_2124)">
-                          <g filter="url(#filter0_f_99_2124)">
-                            <ellipse
-                              cx="45"
-                              cy="57"
-                              rx="27.5"
-                              ry="27.5"
-                              fill="#2398FF"
-                              fillOpacity="0.59"
-                            />
-                          </g>
+                    <g opacity="0.21">
+                      <mask
+                        id="mask0_99_2124"
+                        style={{ maskType: "alpha" }}
+                        maskUnits="userSpaceOnUse"
+                        x="0"
+                        y="0"
+                        width="90"
+                        height="50"
+                      >
+                        <rect width="90" height="50" fill="#D9D9D9" />
+                      </mask>
+                      <g mask="url(#mask0_99_2124)">
+                        <g filter="url(#filter0_f_99_2124)">
+                          <ellipse
+                            cx="45"
+                            cy="57"
+                            rx="27.5"
+                            ry="27.5"
+                            fill="#2398FF"
+                            fillOpacity="0.59"
+                          />
                         </g>
                       </g>
-                      <rect
-                        y="49.1665"
-                        width="90"
-                        height="0.833333"
-                        fill="url(#paint0_linear_99_2124)"
-                      />
-                      <defs>
-                        <filter
-                          id="filter0_f_99_2124"
-                          x="-12.5"
-                          y="-0.5"
-                          width="115"
-                          height="115"
-                          filterUnits="userSpaceOnUse"
-                          colorInterpolationFilters="sRGB"
-                        >
-                          <feFlood floodOpacity="0" result="BackgroundImageFix" />
-                          <feBlend
-                            mode="normal"
-                            in="SourceGraphic"
-                            in2="BackgroundImageFix"
-                            result="shape"
-                          />
-                          <feGaussianBlur
-                            stdDeviation="15"
-                            result="effect1_foregroundBlur_99_2124"
-                          />
-                        </filter>
-                        <linearGradient
-                          id="paint0_linear_99_2124"
-                          x1="0"
-                          y1="49.5832"
-                          x2="90"
-                          y2="49.5832"
-                          gradientUnits="userSpaceOnUse"
-                        >
-                          <stop stopColor="#2398FF" stopOpacity="0" />
-                          <stop offset="0.5" stopColor="#2398FF" />
-                          <stop offset="1" stopColor="#2398FF" stopOpacity="0" />
-                        </linearGradient>
-                      </defs>
-                    </svg>
-                  ) : null}
-                </div>
-              ))}
-            </nav>
+                    </g>
+                    <rect
+                      y="49.1665"
+                      width="90"
+                      height="0.833333"
+                      fill="url(#paint0_linear_99_2124)"
+                    />
+                    <defs>
+                      <filter
+                        id="filter0_f_99_2124"
+                        x="-12.5"
+                        y="-0.5"
+                        width="115"
+                        height="115"
+                        filterUnits="userSpaceOnUse"
+                        colorInterpolationFilters="sRGB"
+                      >
+                        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                        <feBlend
+                          mode="normal"
+                          in="SourceGraphic"
+                          in2="BackgroundImageFix"
+                          result="shape"
+                        />
+                        <feGaussianBlur stdDeviation="15" result="effect1_foregroundBlur_99_2124" />
+                      </filter>
+                      <linearGradient
+                        id="paint0_linear_99_2124"
+                        x1="0"
+                        y1="49.5832"
+                        x2="90"
+                        y2="49.5832"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop stopColor="#2398FF" stopOpacity="0" />
+                        <stop offset="0.5" stopColor="#2398FF" />
+                        <stop offset="1" stopColor="#2398FF" stopOpacity="0" />
+                      </linearGradient>
+                    </defs>
+                  </svg>
+                ) : null}
+              </div>
+            ))}
+          </nav>
 
-            {faqs
-              ?.filter((faq: any) => faq.category === active)
-              ?.map((item: any) => (
-                <Link
-                  key={item.id.toString()}
-                  className="flex w-full flex-col gap-y-2.5 border-b border-gray-200 py-[15px]"
-                  href={`/faq/${item.id}`}
-                >
-                  <div className="grid grid-cols-[45px_1fr] items-center gap-x-2.5">
-                    <div
-                      className={`rounded-full px-2.5 text-sm font-semibold ${item.subcategory === "공지" ? "bg-serve text-gray-100" : "bg-primary text-gray-700"}`}
-                    >
-                      {item.subcategory}
-                    </div>
-                    <div className="truncate text-base font-medium leading-tight text-gray-700">
-                      {item.title}
-                    </div>
+          {faqs
+            ?.filter((faq: any) => faq.category === active)
+            ?.map((item: any) => (
+              <Link
+                key={item.id.toString()}
+                className="flex w-full flex-col gap-y-2.5 border-b border-gray-200 py-[15px]"
+                href={`/faq/${item.id}`}
+              >
+                <div className="grid grid-cols-[45px_1fr] items-center gap-x-2.5">
+                  <div
+                    className={`rounded-full px-2.5 text-sm font-semibold ${item.subcategory === "공지" ? "bg-serve text-gray-100" : "bg-primary text-gray-700"}`}
+                  >
+                    {item.subcategory}
                   </div>
-                  <div className="flex items-center gap-x-[5px]">
-                    <Image src="/clock.png" alt="작성일" width={14} height={14} />
-                    <div className="text-sm font-normal text-gray-500">
-                      {changeDate(item.created_at)}
-                    </div>
+                  <div className="truncate text-base font-medium leading-tight text-gray-700">
+                    {item.title}
                   </div>
-                </Link>
-              ))}
-          </div>
+                </div>
+                <div className="flex items-center gap-x-[5px]">
+                  <Image src="/clock.png" alt="작성일" width={14} height={14} />
+                  <div className="text-sm font-normal text-gray-500">
+                    {changeDate(item.created_at)}
+                  </div>
+                </div>
+              </Link>
+            ))}
         </div>
       </section>
 
       {/* 데스크탑(≥1024)에서만 보임 */}
-      <section className="relative hidden desktop:block desktop:aspect-[1920/2086] desktop:w-full desktop:bg-[url('/bg_desktop.png')] desktop:bg-[length:100%_auto] desktop:bg-top desktop:bg-repeat-y">
-        <TopBar />
-
-        <div className="desktop:flex desktop:flex-col desktop:items-center desktop:pt-60">
+      <section className="hidden desktop:mt-60 desktop:block desktop:min-h-screen desktop:flex-1 ">
+        <div className="desktop:mx-auto desktop:flex desktop:w-full desktop:max-w-[1200px] desktop:flex-col">
           <h1 className="desktop:text-center desktop:text-5xl desktop:font-bold desktop:text-white">
             자주 묻는 질문
           </h1>
-
-          <div className="min-h-[1150px] w-[1200px] desktop:mt-20 desktop:flex desktop:flex-col desktop:rounded-[25px] desktop:bg-[#16172D]/70 desktop:px-[50px] desktop:py-5">
+          <div className="mb-20 min-h-[1150px] desktop:mt-20 desktop:flex desktop:flex-col desktop:rounded-[25px] desktop:bg-[#16172D]/70 desktop:px-[50px] desktop:pb-5 desktop:pt-[30px]">
             <nav className="desktop:flex">
               {tabs.map((tab) => (
                 <div key={tab} className="relative">

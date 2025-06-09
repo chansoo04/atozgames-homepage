82d8626e9e644a2fbad1208e32de2807a1ce2604
chansoo04 <56948241+chansoo04@users.noreply.github.com>
2025-06-09 22:56:18 +0900

    [fix] 데스크탑 계산기 추가

--- apps/admin/app/_components/sizeCalculator.ts
+++ apps/admin/app/_components/sizeCalculator.ts
@@ -1,13 +1,31 @@
-const 기준비율 = 2.1;
-
 // TODO: 사이즈 맞춰서 수정 필요
-export const mobileSizeCalc = (크기: number, ratio: number) => {
+// 기준 비율: 360 * 783
+export const mobileSizeCalc = (크기: number, ratio: number, 기준비율?: number) => {
+  if (기준비율 === undefined || 기준비율 === null) {
+    기준비율 = 2.1;
+  }
   if (ratio < 기준비율) {
     const 새로운크기 = Math.round((크기 / 783) * 10000) / 100;
     return `${새로운크기}dvh`;
   }
   if (ratio >= 기준비율) {
     const 새로운크기 = Math.round((크기 / 360) * 10000) / 100;
+    return `${새로운크기}dvw`;
+  }
+};
+
+// 기준 비율: 1920 * 1080
+export const desktopSizeCalc = (크기: number, ratio: number, 기준비율?: number) => {
+  if (기준비율 === undefined || 기준비율 === null) {
+    기준비율 = 1.78;
+  }
+
+  if (ratio < 기준비율) {
+    const 새로운크기 = Math.round((크기 / 1080) * 10000) / 100;
+    return `${새로운크기}dvh`;
+  }
+  if (ratio >= 기준비율) {
+    const 새로운크기 = Math.round((크기 / 1920) * 10000) / 100;
     return `${새로운크기}dvw`;
   }
 };

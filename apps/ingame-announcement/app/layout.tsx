ea2e84b3a9428d02af3941c983c7055f79dd42bd
chansoo04 <56948241+chansoo04@users.noreply.github.com>
2025-08-01 22:08:25 +0900

    [new] 테스트 버전 배포4

--- apps/ingame-announcement/app/layout.tsx
+++ apps/ingame-announcement/app/layout.tsx
@@ -12,9 +12,17 @@ export const viewport: Viewport = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html className="bg-amber-400">
+    <html
+      // className="bg-amber-400"
+      style={{
+        backgroundImage: `url('/background.png')`,
+        backgroundSize: "100% auto", // 가로 기준 꽉 채움
+        backgroundPosition: "center center", // 세로 중앙 정렬
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <body
-        className="bg-red-700"
+        className="bg-red-300"
         style={{
           margin: "0 auto",
           // width: "100%",

99dba3a788f001748d9655f78a311202a993d2ac
chansoo04 <56948241+chansoo04@users.noreply.github.com>
2025-08-12 20:53:28 +0900

    [ingame-announcement] assertion 제거

--- apps/ingame-announcement/lib/fetcher/noAuth.ts
+++ apps/ingame-announcement/lib/fetcher/noAuth.ts
@@ -8,8 +8,7 @@ export default async function noAuth<T>(
   path: string,
   { revalidate }: { revalidate: boolean } = { revalidate: true },
 ): Promise<T> {
-  assert(process.env.NEXT_PUBLIC_API_URL, "NEXT_PUBLIC_API_URL is not set");
-  const url = new URL(path, process.env.NEXT_PUBLIC_API_URL);
+  const url = new URL(path, process.env.NEXT_PUBLIC_SELF_URL);
 
   // 헤더에 다른 값을 넣으면 next.revalidate설정이 안 먹는 것 같다
   // 그래서 캐싱 문제가 발생해서 결제 페이지등에서 결제금액 불일치 오류가 날 수 있다
@@ -28,7 +27,7 @@ export default async function noAuth<T>(
 }
 
 export const nfetch = async <T>(path: string, init?: RequestInit) => {
-  const url = new URL(path, process.env.NEXT_PUBLIC_API_URL);
+  const url = new URL(path, process.env.NEXT_PUBLIC_SELF_URL);
   const res = await fetch(url, init);
 
   if (!res.ok) {

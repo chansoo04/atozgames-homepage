ca49b600c6cc668fe473e1bd9f153d407cf090fa
chansoo04 <56948241+chansoo04@users.noreply.github.com>
2025-09-26 19:32:53 +0900

    오류 확인을 위한 커밋4

--- apps/identity-verification/app/identity-verification/page.tsx
+++ apps/identity-verification/app/identity-verification/page.tsx
@@ -53,7 +53,10 @@ export default function Page() {
       try {
         win.MOBILEOK.process(url, "MWV", "result");
       } catch (error) {
-        alert("MOK 인증을 시작하는데 실패했습니다. 팝업이 허용되어 있는지 확인해주세요.");
+        alert(
+          "MOK 인증을 시작하는데 실패했습니다. 팝업이 허용되어 있는지 확인해주세요." +
+            JSON.stringify(error),
+        );
         setShow(true);
         return Promise.reject(new Error("MOK 인증을 시작하는데 실패했습니다."));
       }
